fb83d9e3bd121ae1cf8e6d9ce5e7e070302fcfe5
ical <nukical@gmail.com>
2022-05-30 22:50:35 +0800

    the number was wrong

--- src/reinforcement_learning_planner/lib/rl_handler/rl_handler_lib.cpp
+++ src/reinforcement_learning_planner/lib/rl_handler/rl_handler_lib.cpp
@@ -413,7 +413,7 @@ void RL_handler::rand_action()
 
     if(angular == 0)
     {
-        if (linear == 2 || linear == -2)
+        if (linear == 2 || linear == 0)
         {
             rand_action();
             return;
