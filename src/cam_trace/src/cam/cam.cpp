1b220f00486da25f959e5fcccf17dee4ba660318
ical <nukical@gmail.com>
2022-06-09 21:17:59 +0800

    added option and accidental format

--- src/cam_trace/src/cam/cam.cpp
+++ src/cam_trace/src/cam/cam.cpp
@@ -1,22 +1,31 @@
 #include "cam/cam.h"
 #include "ltproc/ltproc.h"
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "offset_pub");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub;
     Cam cam(nh);
     ltproc ltproc(nh);
-    ros::Rate loop_rate(30);
- //   sub = it.subscribe("/camera/infra/image_raw", 10, &Cam::Callback, &cam);
-    while(ros::ok())
+
+    bool show_result = false;
+    int cam_rate = 10;
+
+    nh.getParam("show_result", show_result);
+    nh.getParam("cam_rate", cam_rate);
+
+    ros::Rate loop_rate(cam_rate);
+    //   sub = it.subscribe("/camera/infra/image_raw", 10, &Cam::Callback, &cam);
+    while (ros::ok())
     {
-         cam.Capture();
+        cam.Capture();
         ltproc.Capture(cam.frame);
-         ltproc.set_window(640,480);
-         ltproc.lt_proc();
-        // ltproc.show_result(240,180);
-        if(cv::waitKey(5) == 27)
+        ltproc.set_window(640, 480);
+        ltproc.lt_proc();
+        if (show_result)
+            ltproc.show_result(240, 180);
+        if (cv::waitKey(5) == 27)
         {
             break;
         }
@@ -25,4 +34,3 @@ int main(int argc, char** argv){
     }
     return 0;
 }
-
